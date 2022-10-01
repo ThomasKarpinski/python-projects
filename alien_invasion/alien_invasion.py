259b05d8c90b1d3c131ac2e7b0c8fb8ab349e4ca
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2022-10-01 22:47:10 +0200

    new bullet object

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,19 +3,20 @@ import sys
 import pygame
 
 from settings import Settings
-
 from ship import Ship
 
 
 class AlienInvasion:
-    """Ogólna klasa przeznaczona do zarządania zasobami i sposobem działania gry."""
+    """Ogólna klasa przeznaczona do zarządzania zasobami i sposobem działania gry."""
 
     def __init__(self):
         """Inicjalizacja gry i utworzenie jej zasobów."""
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Inwazja obcych")
 
         self.ship = Ship(self)
@@ -24,6 +25,7 @@ class AlienInvasion:
         """Rozpoczęcie pętli głównej gry."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -31,19 +33,36 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Reakcja na naciśnięcie klawisza."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Reakcja na zwolnienie klawisza."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
-        """Uaktualnianie obrazów na ekranie i przejście do nowego ekranu."""
-
-        # Odświeżenie ekranu w trakcie każdej iteracji pętli
+        """Uaktualnienie obrazów na ekranie i wyświetlenie tego ekranu."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
 
-        # Wyświetlanie ostatnio zmodyfikowanego ekranu
         pygame.display.flip()
 
 
 if __name__ == '__main__':
-    # Utworzenie egzemplarza gry i jej uruchomienie
+    # Utworzenie egzemplarza gry i jej uruchomienie.
     ai = AlienInvasion()
     ai.run_game()
---
+++ alien_invasion/bullet.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """Klasa przeznaczona do zarządzania pociskami wystrzeliwanymi przez statek."""
+
+    def __init__(self, ai_game):
+        """Utworzenie obiektu pocisku w aktualnym położeniu statku"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = ai_game.settings.bullet_color
+
+        # Utworzenie postoąta pocisku w punkcie (0,0), a następnie
+        # zdefiniowanie dla niego odpowiedniego położenia
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+                                self.settings.bullet_height)
+        self.rect.midbottom = ai_game.ship.rect.midtop
+
+        # Położenie pocisku jest zdefiniowane za pomocą wartości zmiennoprzecinkowej
+        self.y = float(self.rect.y)
